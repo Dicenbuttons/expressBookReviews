421ad6a7cac2f280ad522adc7e5a86a72e29f281
Carson Hill <dicenbuttons@gmail.com>
2025-05-23 14:37:56 -0400

    task 1 commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,10 +11,10 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/', function (req, res) {
+    // Use JSON.stringify to format the output neatly
+    return res.status(200).json(JSON.stringify(books, null, 2));
+  });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
